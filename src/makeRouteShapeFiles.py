7aad59813834f517e6f83ed51b82a9176cf7b11e
dds <11372904+ddstevenson@users.noreply.github.com>
2020-07-07 16:48:35 -0700

    Changed main to explore

--- src/makeRouteShapeFiles.py
+++ src/makeRouteShapeFiles.py
@@ -1,20 +1,26 @@
 from pathlib import Path
 import pandas as pd
-import datetime
-import matplotlib.pyplot as plt
 
-path = Path().joinpath('OriginalData', 'cyclic_data_20200224_0320_wkd')
-files = path.glob("*.tsv")
-li = []
+routes = pd.read_csv(Path().joinpath('OriginalData', 'C-Tran_GTFSfiles_20200105', 'google_transit_20200105',
+                                     'routes.txt'))
+shapes = pd.read_csv(Path().joinpath('OriginalData', 'C-Tran_GTFSfiles_20200105', 'google_transit_20200105',
+                                     'shapes.txt'))
+trips = pd.read_csv(Path().joinpath('OriginalData', 'C-Tran_GTFSfiles_20200105', 'google_transit_20200105',
+                                    'trips.txt'))
 
-for filename in files:
-    df = pd.read_csv(filename, sep='\t', header=0)
-    li.append(df)
+# Explanation: indices get lost in the merge, so we need to dup them
+routes.insert(0, 'route_index', routes['route_id'])
+trips.insert(0, 'shape_index', trips['shape_id'])
 
-crumbs = pd.concat(li, axis=0, ignore_index=True)
-cad_avl = pd.read_csv(Path().joinpath('OriginalData', 'C-Tran_CAD_AVL_trips_Feb+Mar2020', 'C-Tran_CAD_AVL_trips_Feb'
-                                                                                          '+Mar2020.csv'))
-# Set up joins
-crumbs.set_index('EVENT_NO_TRIP')
-cad_avl.set_index('trip_number')
-df = crumbs.join(cad_avl[['trip_number', 'route_number']])
+# Join
+df = routes.set_index('route_id').join(trips.set_index('route_id'))
+df.drop_duplicates(['route_index', 'shape_index'], inplace=True)
+df = df.set_index('shape_id').join(shapes.set_index('shape_id'))
+df = df[['route_index', 'shape_index', 'shape_pt_sequence', 'shape_pt_lat', 'shape_pt_lon']]
+
+# Write each route out to its own file df.sort_values(by=['route_index', 'shape_pt_sequence'], inplace=True)
+vals = df['route_index'].unique()
+for val in vals:
+    df2 = df.query('route_index == @val & shape_pt_sequence == null')
+    df2 = df2.sort_values(by='shape_pt_sequence')
+    df2.to_csv(Path().joinpath('out', 'shapes', str(val) + '.csv'))
