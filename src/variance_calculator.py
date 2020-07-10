559b06b583bfc36bd6270b286d00be6797d60028
ddstevenson <MY_NAME@example.com>
2020-07-09 19:06:14 -0700

    Fixed transformation with pandas & numpy calls

--- src/variance_calculator.py
+++ src/variance_calculator.py
@@ -5,7 +5,7 @@ import numpy
 
 
 def get_distance(x1, y1, x2, y2):
-    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return numpy.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
 
 routes = pd.read_csv(Path().joinpath('OriginalData', 'C-Tran_GTFSfiles_20200105', 'google_transit_20200105',
@@ -18,6 +18,10 @@ trips = pd.read_csv(Path().joinpath('OriginalData', 'C-Tran_GTFSfiles_20200105',
 # Explanation: indices get lost in the merge, so we need to dup them
 routes.insert(0, 'route_index', routes['route_id'])
 trips.insert(0, 'shape_index', trips['shape_id'])
+
+# Clean up this field for upcoming merge with breadcrumb data
+routes['route_short_name'].replace(to_replace='Vine', value=50, inplace=True)
+routes['route_short_name'] = pd.to_numeric(routes['route_short_name'])
 
 # Join
 df = routes.set_index('route_id').join(trips.set_index('route_id'))
@@ -34,19 +38,11 @@ del trips
 del shapes
 
 # Calculate distance (if shapes_distance.csv does not exist)
-df['shape_dist_traveled'] = df['shape_dist_traveled'].mask(df['shape_pt_sequence'] != 0,
-                               numpy.sqrt((df.shift(1)['shape_pt_lat'] - df[
-                                   'shape_pt_lat']) ** 2 + (df.shift(1)['shape_pt_lon'] -
-                                                 df['shape_pt_lon']) ** 2) +
-                                                           df.shift(1)['shape_dist_traveled'])
-
-df.to_csv(Path().joinpath('out', 'shapes_distance.csv'))
-
-# Run these if shapes_distance.csv exists
-df = pd.read_csv(Path().joinpath('out', 'shapes_distance.csv'))
-del df['shape_index']
-df['route_short_name'].replace(to_replace='Vine', value=50, inplace=True)  # already wrote file
-df['route_short_name'] = pd.to_numeric(df['route_short_name'])
+df['shape_dist_traveled'].mask(df['shape_pt_sequence'] != 0,
+                               numpy.sqrt((df.shift(1)['shape_pt_lat'] - df['shape_pt_lat']) ** 2 + (
+                                       df.shift(1)['shape_pt_lon'] -
+                                       df['shape_pt_lon']) ** 2) +
+                               df.shift(1)['shape_dist_traveled'], inplace=True)
 
 # Now grab breadcrumb data
 path = Path().joinpath('OriginalData', 'cyclic_data_20200224_0320_wkd')
@@ -60,6 +56,9 @@ for filename in files:
 crumbs = pd.concat(li, axis=0, ignore_index=True)
 cad_avl = pd.read_csv(Path().joinpath('OriginalData', 'C-Tran_CAD_AVL_trips_Feb+Mar2020', 'C-Tran_CAD_AVL_trips_Feb'
                                                                                           '+Mar2020.csv'))
+del li
+del tmp
+
 # Set up joins
 cad_avl = cad_avl[['trip_id', 'route_number']].drop_duplicates(['trip_id', 'route_number'])
 crumbs.insert(0, 'TRIP_NO', crumbs['EVENT_NO_TRIP'])
