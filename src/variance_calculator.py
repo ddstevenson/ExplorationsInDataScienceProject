9da1040796bbd869cbf76f03a8ec2f9a8b009947
ddstevenson <MY_NAME@example.com>
2020-07-09 18:41:48 -0700

    Fixed transformation with pandas & numpy calls

--- src/variance_calculator.py
+++ src/variance_calculator.py
@@ -1,10 +1,11 @@
 from sympy.geometry import Point
 from pathlib import Path
 import pandas as pd
+import numpy
 
 
 def get_distance(x1, y1, x2, y2):
-    return Point((x1, y1)).distance(Point((x2, y2)))
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
 
 routes = pd.read_csv(Path().joinpath('OriginalData', 'C-Tran_GTFSfiles_20200105', 'google_transit_20200105',
@@ -32,29 +33,37 @@ del routes
 del trips
 del shapes
 
-# Calculate distance (if file does not exist)
-prog = 0
-prev = 0
-for i in range(1, len(df)):
-    x = 0
-    if df.iat[i, df.columns.get_loc('shape_pt_sequence')] != 0:
-        x = get_distance(df.iat[i, df.columns.get_loc('shape_pt_lat')],
-                         df.iat[i, df.columns.get_loc('shape_pt_lon')],
-                         df.iat[i-1, df.columns.get_loc('shape_pt_lat')],
-                         df.iat[i-1, df.columns.get_loc('shape_pt_lon')]) + prev
-    df.iat[i, df.columns.get_loc('shape_dist_traveled')] = x
-    prev = x
-    if prog % 100 == 0:  # About 60k records total
-        print(str(prog) + ' records processed (of ' + str(df['shape_pt_lon'].size)
-              + '): currently ' + str(df.iat[i, df.columns.get_loc('shape_dist_traveled')]))
-    prog += 1
+# Calculate distance (if shapes_distance.csv does not exist)
+df['shape_dist_traveled'] = df['shape_dist_traveled'].mask(df['shape_pt_sequence'] != 0,
+                               numpy.sqrt((df.shift(1)['shape_pt_lat'] - df[
+                                   'shape_pt_lat']) ** 2 + (df.shift(1)['shape_pt_lon'] -
+                                                 df['shape_pt_lon']) ** 2) + df.shift(1)['shape_dist_traveled'])
 
 df.to_csv(Path().joinpath('out', 'shapes_distance.csv'))
 
-# Run these two if file exists
+# Run these if shapes_distance.csv exists
 df = pd.read_csv(Path().joinpath('out', 'shapes_distance.csv'))
 del df['shape_index']
-df['route_short_name'].replace(to_replace='Vine', value=50, inplace=True)   # already wrote file
+df['route_short_name'].replace(to_replace='Vine', value=50, inplace=True)  # already wrote file
 df['route_short_name'] = pd.to_numeric(df['route_short_name'])
 
+# Now grab breadcrumb data
+path = Path().joinpath('OriginalData', 'cyclic_data_20200224_0320_wkd')
+files = path.glob("*.tsv")
+li = []
 
+for filename in files:
+    tmp = pd.read_csv(filename, sep='\t', header=0)
+    li.append(tmp)
+
+crumbs = pd.concat(li, axis=0, ignore_index=True)
+cad_avl = pd.read_csv(Path().joinpath('OriginalData', 'C-Tran_CAD_AVL_trips_Feb+Mar2020', 'C-Tran_CAD_AVL_trips_Feb'
+                                                                                          '+Mar2020.csv'))
+# Set up joins
+cad_avl = cad_avl[['trip_id', 'route_number']].drop_duplicates(['trip_id', 'route_number'])
+crumbs.insert(0, 'TRIP_NO', crumbs['EVENT_NO_TRIP'])
+cad_avl.insert(0, 'trip_no', cad_avl['trip_id'])
+crumbs.set_index('TRIP_NO', inplace=True)
+cad_avl.set_index('trip_no', inplace=True)
+crumbs = crumbs.join(cad_avl)
+del cad_avl
